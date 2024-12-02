028ae136f1688e49ff69d47b196e846a51497bb7
ankurdev002 <ankursaini795@gmail.com>
2024-12-02 23:18:25 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import "./App.css"; // Assuming you will create a separate CSS file for styles
 
 const App = () => {
   return (
-    <div>
-      <div className="navbar">
+    <div className="m-4 bg-white rounded-3xl overflow-hidden h-screen">
+      <div className="navbar !p-8">
         <div className="logo">
           <img
             alt="Logo"
@@ -25,36 +24,52 @@ const App = () => {
           See pricing
         </a>
       </div>
-      <div className="content">
-        <div className="beta">
-          <p>BETA 2.0 AVAILABLE NOW</p>
-        </div>
-        <h1>
-          Build websites <span className="highlight">effortlessly</span> now or
-          never!
-        </h1>
-        <p>
-          Easy-to-use and powerful page builder, your gateway to intuitive tools
-          and expansive customization options that transform how you build
-          websites.
-        </p>
-        <a className="cta" href="#">
-          Get started <i className="fas fa-arrow-right"></i>
-        </a>
-        <p>No credit cards required - 14 days free trial.</p>
-        <div className="avatars">
-          <img
-            alt="Avatar 1"
-            height="50"
-            src="https://storage.googleapis.com/a1aa/image/Umk6a9tp1LKQFNsRUWBPZWc2fbLBJGcM1wJkW2Q2O7MzOR7JA.jpg"
-            width="50"
-          />
-          <img
-            alt="Avatar 2"
-            height="50"
-            src="https://storage.googleapis.com/a1aa/image/U5QZy2030a5KHBhmwpfA6XqJj8ErGfLXBsLRsiVvUN5odi2TA.jpg"
-            width="50"
-          />
+      <div className="bg-gradient-to-tr from-[#0a0f1e] to-[#081f2e] min-h-screen flex justify-start py-16 items-center flex-col w-full">
+        <div className="flex flex-col items-center justify-center">
+          <div className="text-center slide-in-bottom-delay-100 slide-in-bottom">
+            <p className="px-5 py-2 border rounded-full">
+              BETA 2.0 AVAILABLE NOW
+            </p>
+          </div>
+          <div className="max-w-50 slide-in-bottom-delay-200 slide-in-bottom pt-8">
+            <h1 className="text-center !text-[5rem] font-bold leading-tight">
+              Build websites
+              <br />
+              <span className="bg-[#f1e2de] !text-[#12715b] inline-flex px-1 rounded-2xl -rotate-2">
+                effortlessly
+              </span>
+              <br />
+              now or never!
+            </h1>
+          </div>
+          <p className="slide-in-bottom-delay-300 slide-in-bottom pt-4">
+            Easy-to-use and powerful page builder, your gateway to intuitive
+            tools and expansive customization options that transform how you
+            build websites.
+          </p>
+          <a
+            className="cta slide-in-bottom-delay-400 slide-in-bottom my-8"
+            href="#"
+          >
+            Get started <i className="fas fa-arrow-right"></i>
+          </a>
+          <p className="slide-in-bottom-delay-500 slide-in-bottom">
+            No credit cards required - 14 days free trial.
+          </p>
+          <div className="avatars flex slide-in-bottom-delay-600 slide-in-bottom">
+            <img
+              alt="Avatar 1"
+              height="50"
+              src="https://storage.googleapis.com/a1aa/image/Umk6a9tp1LKQFNsRUWBPZWc2fbLBJGcM1wJkW2Q2O7MzOR7JA.jpg"
+              width="50"
+            />
+            <img
+              alt="Avatar 2"
+              height="50"
+              src="https://storage.googleapis.com/a1aa/image/U5QZy2030a5KHBhmwpfA6XqJj8ErGfLXBsLRsiVvUN5odi2TA.jpg"
+              width="50"
+            />
+          </div>
         </div>
       </div>
     </div>
